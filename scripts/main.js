4d9ae14452c886ba57f06f2156d05f57a48d3717
Pallie-01 <36259190+Pallie-01@users.noreply.github.com>
2018-03-02 15:57:36 +0200

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,28 @@
-var myButton = document.getElementById('userbutton');
-var headingOne = document.querySelector('h1');
-
-function setUserName() {
-  var myName = prompt('Please enter your name.');
-  localStorage.setItem('name', myName);
-  headingOne.textContent = 'Hi ' + myName + '. Welcome to my profile.';
-}
-
-if(!localStorage.getItem('name')) {
-  setUserName();
-} else {
-  var storedName = localStorage.getItem('name');
-  headingOne.textContent = 'Hi ' + storedName + '.Welcome to my profile.';
-}
-
-myButton.onclick = function() {
-  setUserName();
-}
-
-function myMap() {
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom: 10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-var map = new google.maps.Map(document.getElementById("map"), mapOptions);
-}
+// var myButton = document.getElementById('userbutton');
+// var headingOne = document.querySelector('h1');
+//
+// function setUserName() {
+//   var myName = prompt('Please enter your name.');
+//   localStorage.setItem('name', myName);
+//   headingOne.textContent = 'Hi ' + myName + '. Welcome to my profile.';
+// }
+//
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   var storedName = localStorage.getItem('name');
+//   headingOne.textContent = 'Hi ' + storedName + '.Welcome to my profile.';
+// }
+//
+// myButton.onclick = function() {
+//   setUserName();
+// }
+//
+// function myMap() {
+//     var mapOptions = {
+//         center: new google.maps.LatLng(51.5, -0.12),
+//         zoom: 10,
+//         mapTypeId: google.maps.MapTypeId.HYBRID
+//     }
+// var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+// }
